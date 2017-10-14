514af403f12e1002bf713ac2e7ac64639e77277e
ajpershick <ajpershick@gmail.com>
2017-10-14 01:19:57 -0700

    fix validation error

--- app/controllers/enrolls_controller.rb
+++ app/controllers/enrolls_controller.rb
@@ -25,8 +25,8 @@ class EnrollsController < ApplicationController
   # POST /enrolls.json
   def create
     custom_params = enroll_params
-    custom_params[:student_id] = Student.find_by(student_id: custom_params[:student_id]).id
-    custom_params[:course_id] = Course.find_by(course_id: custom_params[:course_id]).id
+    custom_params[:student_id] = Student.find_by(student_id: custom_params[:student_id])&.id
+    custom_params[:course_id] = Course.find_by(course_id: custom_params[:course_id])&.id
     @enroll = Enroll.new(custom_params)
 
     respond_to do |format|
